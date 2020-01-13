ef33f8f51b55ef49377e1dd80a4225dd4117082a
Jhonatan de Souza <jdsjhonatan@hotmail.com>
2020-01-13 18:47:55 -0300

    projeto 1 e 2

---
+++ simulador_de_dado.py
@@ -0,0 +1,36 @@
+# Simulador de dado
+# Simular o uso de um dado, gerando um valor de 1 até 6
+import random
+import PySimpleGUI as sg
+
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        # Layout
+        self.layout = [
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('sim'),sg.Button('Não')]
+        ]
+    
+    def Iniciar(self):
+        # criar uma janela
+        self.janela = sg.Window('Simulador de Dado',layout=self.layout)
+        # ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()
+        # fazer alguma coisa com esses valores
+        try:
+            if self.eventos == 'sim' or self.eventos == 's':
+                self.GerarValorDoDado()
+            elif self.eventos == 'Não' or self.eventos == 'n':
+                print('Agrecemos a sua participação!')
+            else:
+                print('Favor digitar sim ou não')
+        except:
+            print('Ocorreu um erro ao receber sua resposta')
+    
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo,self.valor_maximo))
+
+simulador = SimuladorDeDado()
+simulador.Iniciar()
